0f5f945c35d6af1de78d287fa899ec09838d6b2b
Nikita Dubinin <nik.dubinin.01@mail.ru>
2023-10-13 20:50:28 +0300

    добавил значение input в свойство square

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+"use strict";
 
+const input = document.getElementById("text");
+const button = document.getElementById("btn");
+const square = document.getElementById("square");
 
+console.dir(square);
 
+button.addEventListener("click", () => {
+  square.style.backgroundColor = input.value;
+});
 
-
+console.log(input);

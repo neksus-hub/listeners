7a055278e7b0fae97b5804d8e5089d9aa5a7ab4e
Nikita Dubinin <nik.dubinin.01@mail.ru>
2023-10-13 21:10:58 +0300

    передал в span текстовое значение range

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const input = document.getElementById("text");
 const button = document.getElementById("btn");
 const square = document.getElementById("square");
 const btnInCircle = document.getElementById("e_btn");
+const range = document.getElementById("range");
+const rangeSpan = document.getElementById("range-span");
 
 console.dir(square);
 
@@ -11,6 +13,11 @@ button.addEventListener("click", () => {
   square.style.backgroundColor = input.value;
 });
 
+const logger = function (event) {
+  rangeSpan.textContent = event.target.value;
+};
+
 btnInCircle.classList.add("display-none");
+range.addEventListener("input", logger);
 
 console.log(btnInCircle);
